5f786241ef9967c163d72015924b9140ea28bb78
eugene.bulkin <ebulkin@caltech.edu>
2014-02-15 17:40:35 -0600

    Finish plot spec

--- src/Game/Error.ts
+++ src/Game/Error.ts
@@ -14,8 +14,12 @@ module Game {
      * @param {string} message
      * @constructor
      */
-    constructor(message?: string) {
-      this.message = message;
+    constructor(message?: string, ...data: any[]) {
+      // replace {{n}} with data[n-1] everywhere
+      message = message || "";
+      this.message = message.replace(/{{(\d+)}}/g, function(match, id) {
+        return data[id - 1] || "";
+      });
       this.stack = (new Error()).stack;
     }
 
@@ -33,7 +37,10 @@ module Game {
       NOT_OBJECT: "You must call this method with a Game.Object instance.",
       ALREADY_ADDED: "Object already added to state.",
       NOT_ADDED: "Object was not added to state.",
-      BAD_SCORE_DATA: "Score can only change by numerical amounts."
+      BAD_SCORE_DATA: "Score can only change by numerical amounts.",
+      LEVEL_NOT_EXIST: "Specified level {{1}} does not exist.",
+      REMOVE_CUR_LEVEL: "Cannot remove current level.",
+      ADVANCE_CUR_LEVEL: "Cannot advance to current level."
     };
   }
 }
--- src/Game/Plot.ts
+++ src/Game/Plot.ts
@@ -1,4 +1,5 @@
 ///<reference path="./Utils.ts" />
+///<reference path="./Error.ts" />
 ///<reference path="./State.ts" />
 ///<reference path="./StateTracker.ts" />
 
@@ -31,17 +32,21 @@ module Game {
 
     public removeLevel(id: number): void {
       if(id < 0 || id >= this.levels.length) {
-        // throw error here
+        throw new GameError(GameError.ErrorType.LEVEL_NOT_EXIST, id);
       }
       if(id === this.currentLevel) {
-        // throw error here
+        throw new GameError(GameError.ErrorType.REMOVE_CUR_LEVEL);
       }
       this.levels.splice(id, 1);
+      // if the id was below the current level, maintain the correct order!
+      if(id < this.currentLevel) {
+        this.currentLevel -= 1;
+      }
     }
 
     public addGoals(goals: Requirement[], id?: number): void {
       if(id < 0 || id >= this.levels.length) {
-        // throw error here
+        throw new GameError(GameError.ErrorType.LEVEL_NOT_EXIST, id);
       }
       id = id || this.currentLevel;
       this.levels[id].goals.push.apply(this.levels[id].goals, goals);
@@ -49,7 +54,7 @@ module Game {
 
     public addEffects(effects: Effect[], id?: number): void {
       if(id < 0 || id >= this.levels.length) {
-        // throw error here
+        throw new GameError(GameError.ErrorType.LEVEL_NOT_EXIST, id);
       }
       id = id || this.currentLevel;
       this.levels[id].effects.push.apply(this.levels[id].effects, effects);
@@ -58,18 +63,60 @@ module Game {
     public advance(id?: number): void {
       id = id || this.currentLevel + 1;
       if(id < 0 || id >= this.levels.length) {
-        // throw error here
+        throw new GameError(GameError.ErrorType.LEVEL_NOT_EXIST, id);
       }
       if(id === this.currentLevel) {
-        // throw error here
+        throw new GameError(GameError.ErrorType.ADVANCE_CUR_LEVEL);
       }
       this.currentLevel = id;
       this.fire('advance', this.currentLevel);
     }
 
+    private satisfied(req: Requirement): boolean {
+      if(!req.name) {
+        return false;
+      }
+      // return false if they specify a prereq without a time period
+      if(req.prereq && !req.within) {
+        return false;
+      }
+      if(!this.eventCounters.hasOwnProperty(req.name)) {
+        return false;
+      }
+      var counter: Counter[] = this.eventCounters[req.name];
+      // set default count
+      var count = (req.count && req.count > 0) ? req.count : 1;
+      // filter out events that don't match the required data
+      if(req.data) {
+        counter = counter.filter(function(e: Counter) {
+          return Utils.matchData(e.data, req.data);
+        }, this);
+      }
+      // if there's a time, filter out all events that occurred before the last
+      // however many milliseconds
+      if(req.within) {
+        var origin = new Date();
+        if(req.prereq) {
+          if(!this.eventCounters[req.prereq]) {
+            return false;
+          }
+          origin = this.eventCounters[req.prereq].slice(-1)[0].time;
+        }
+        counter = counter.filter(function(e: Counter) {
+          return (origin.getTime() - e.time.getTime() <= req.within);
+        });
+      }
+      return (counter.length >= count);
+    }
+
     public onEvent(e: Event): void {
       super.onEvent(e);
       // check goals of current level
+      var goalsAchieved = this.levels[this.currentLevel].goals.every(this.satisfied, this);
+      if(goalsAchieved) {
+        this.fire('levelCompleted', this.currentLevel);
+        this.eventCounters = {};
+      }
     }
   }
 }
